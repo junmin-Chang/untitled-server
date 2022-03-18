170fde4e795a742436fc7cf44890a079166fa367
z00mni <chjm219@kookmin.ac.kr>
2022-03-19 02:28:30 +0900

    댓글 추가, 삭제

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Comment, User } from '@prisma/client';
+import AuthUser from 'src/common/decorators/auth-user.decorator';
+import { CommentService } from './comment.service';
+import { AddCommentDto } from './dto/AddCommentDto';
 
 @Controller('comment')
-export class CommentController {}
+export class CommentController {
+  constructor(private readonly commentService: CommentService) {}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post(':postId')
+  async addComment(
+    @Body() addCommentDto: AddCommentDto,
+    @Param('postId') postId,
+    @AuthUser() user: User,
+  ): Promise<Comment> {
+    return await this.commentService.addComment(postId, addCommentDto, user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete(':commentId')
+  async deleteComment(@Param('commentId') commentId: string): Promise<Comment> {
+    return this.commentService.deleteComment(commentId);
+  }
+}
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { Comment, User } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { AddCommentDto } from './dto/AddCommentDto';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(private prismaService: PrismaService) {}
+
+  async addComment(
+    postId: string,
+    addCommentDto: AddCommentDto,
+    user: User,
+  ): Promise<Comment> {
+    return await this.prismaService.comment.create({
+      data: {
+        userId: user.userId,
+        postId,
+        ...addCommentDto,
+      },
+    });
+  }
+
+  async deleteComment(commentId: string): Promise<Comment> {
+    const isValid = this.prismaService.comment.findUnique({
+      where: {
+        id: commentId,
+      },
+    });
+    if (isValid) {
+      return this.prismaService.comment.delete({
+        where: {
+          id: commentId,
+        },
+      });
+    }
+
+    throw new HttpException('부적절한 접근', 404);
+  }
+}
